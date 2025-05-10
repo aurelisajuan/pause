dad9c76128e2c8196e7ba97d59f69e8523f76a50
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-05-10 12:58:40 -0700

    testing pages

---
+++ entrypoints/landing/index.tsx
@@ -0,0 +1,9 @@
+"use client"; 
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+// Create root element
+const root = document.createElement("div");
+root.id = "pause-root";
+document.body.appendChild(root);
