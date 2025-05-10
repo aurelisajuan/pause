c707d5ea7ce1bb0624ac83795d165e5a0d89e933
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-05-10 16:33:53 -0700

    update: html works

--- src/popup/scripts.ts
+++ src/popup/scripts.ts
@@ -6,7 +6,7 @@ import "./styles.css";
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
-  <React.StrictMode>
-    <PopupApp />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(PopupApp)
+  )
 );
