ec413e866210eaa4ac18e78f39ae5e57ff2b1b95
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-05-10 12:12:34 -0700

    fix: files conf

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,16 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
+
+  // Relative to project root
+  srcDir: "src",             // default: "."
+  outDir: "dist",            // default: ".output"
+
+  // Relative to srcDir
+  entrypointsDir: "entries", // default: "entrypoints"
+  
+  // Configure asset handling
+  vite: () => ({
+    publicDir: 'assets'
+  })
 });
